7c92bda6d5a7371733380f30a8e4f728ebfa785a
Eng.Saif Eldeen <es.seifeldin.shady2023@alexu.edu.eg>
2026-08-23 18:31:03 +0300

    enhancing ui of video projects

--- src/data/videoProjects.js
+++ src/data/videoProjects.js
@@ -1,11 +1,12 @@
-import heroImage from "../assets/hero.png";
-import careerTimelineImage from "../assets/career_axis_timeline.png";
-import speakerCareerImage from "../assets/Speaker_career_axis.png";
-import careerPartnersImage from "../assets/career_axis_parteners.png";
-import iotPosterImage from "../assets/IOT_POSTER.png";
-import mindmapImage from "../assets/mindmap.png";
-import starLogoImage from "../assets/Star++_logo.png";
-import mobileLogoImage from "../assets/موبايلك_logo.png";
+import one from "../assets/hero.png";
+import two from "../assets/career_axis_timeline.png";
+import three from "../assets/Speaker_career_axis.png";
+import four from "../assets/career_axis_parteners.png";
+import five from "../assets/IOT_POSTER.png";
+import six from "../assets/mindmap.png";
+import seven from "../assets/Star++_logo.png";
+import eight from "../assets/موبايلك_logo.png";
+import nine from "../assets/موبايلك_logo.png";
 
 export const VIDEO_PROJECTS = [
   {
@@ -13,7 +14,7 @@ export const VIDEO_PROJECTS = [
     client: "UCCD — Faculty of Engineering",
     description: "A Professional opening video edit for the Jobfair 2025 event, designed to set the tone and engage the audience.",
     tools: ["DaVinci Resolve", "Canva"],
-    image: heroImage,
+    image: one,
     link: "https://drive.google.com/file/d/1fzu_DVpgyM8XCAPt3-MTsh8WwTM8w_IJ/view?usp=drive_link",
   },
   {
@@ -21,7 +22,7 @@ export const VIDEO_PROJECTS = [
     client: "UCCD — Faculty of Engineering",
     description: "A teaser video edit designed to generate excitement and anticipation for the Jobfair 2025 event.",
     tools: ["Video Guro", "Picsart", "Canva"],
-    image: careerTimelineImage,
+    image: two,
     link: "https://drive.google.com/file/d/1a6iVRmZeHszIQ73_vCXBRFkQIf35Yrpq/view?usp=drive_link",
   },
   {
@@ -29,7 +30,7 @@ export const VIDEO_PROJECTS = [
     client: "UCCD — Faculty of Engineering",
     description: "A professional video edit highlighting the CO-OP Ceremony, with emphasis on storytelling and visual composition.",
     tools: ["CapCut", "VN"],
-    image: speakerCareerImage,
+    image: three,
     link: "https://drive.google.com/file/d/1Df5kBVoWz-uRwdwLpBLsRoGLoMzrPJyo/view?usp=drive_link",
   },
   {
@@ -37,7 +38,7 @@ export const VIDEO_PROJECTS = [
     client: "elFath Private School",
     description: "A heartfelt video edit celebrating teachers, with a focus on emotional storytelling and visual composition.",
     tools: ["Video Guro", "Picsart", "Canva"],
-    image: careerPartnersImage,
+    image: four,
     link: "https://drive.google.com/file/d/1BcZdSEPztJdrREf7YZXJIXJ-82H8xFRd/view?usp=drive_link",
   },
   {
@@ -45,7 +46,7 @@ export const VIDEO_PROJECTS = [
     client: "English With Jasmine",
     description: "A compelling narrative showcasing the journey and achievements of a success story of miss yasmin.",
     tools: ["CapCut", "Canva"],
-    image: iotPosterImage,
+    image: five,
     link: "https://drive.google.com/file/d/1I1F1e3PQm3jntkUBT8m2RoQ3UIIu_aCC/view?usp=drive_link",
   },
   {
@@ -53,7 +54,7 @@ export const VIDEO_PROJECTS = [
     client: "personal project",
     description: "A visually engaging video edit capturing the highlights of the Book Fair 2025 at Bibliotheca Alexandrina.",
     tools: ["CapCut", "Canva"],
-    image: mindmapImage,
+    image: six,
     link: "https://drive.google.com/file/d/139SQCUn9HYR3TGQUqBqeIAvlltptQF-G/view?usp=drive_link",
   },
   {
@@ -61,7 +62,7 @@ export const VIDEO_PROJECTS = [
     client: "NASA Space Apps - Alexandria",
     description: "A promotional video edit for the Space Apps Challenge 2026, designed to generate excitement and anticipation for the event.",
     tools: ["CapCut", "Canva", "Video Guro"],
-    image: starLogoImage,
+    image: seven,
     link: "https://drive.google.com/file/d/1uIJfK4CuxG1I9UYXP0mUl90j3tikrsbQ/view?usp=drive_link",
   },
   {
@@ -69,7 +70,7 @@ export const VIDEO_PROJECTS = [
     client: "NASA Space Apps - Alexandria",
     description: "A video edit highlighting the key moments and insights from the Space Apps Bootcamp 2026.",
     tools: ["CapCut", "Canva", "Video Guro"],
-    image: mobileLogoImage,
+    image: eight,
     link: "https://drive.google.com/file/d/1nZPg2xzKoKuo7OLCRarewQSpLR0DLQLA/view?usp=drive_link",
   },
   {
@@ -77,7 +78,7 @@ export const VIDEO_PROJECTS = [
     client: "NASA Space Apps - Alexandria",
     description: "A teaser video edit for the Space Apps Challenge 2026, designed to generate excitement and anticipation for the event.",
     tools: ["CapCut", "Canva", "Video Guro"],
-    image: heroImage,
+    image: nine,
     link: "https://drive.google.com/file/d/1nCneU5VGdL5jc2zclP_eAEoiNH1p942b/view?usp=drive_link",
   },
 ];
